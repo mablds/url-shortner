8969374e730c462dd0e84dca24d44993593cb679
mablds <marcelobragalemos@gmail.com>
2020-10-03 04:02:12 -0300

    ABLE TO FILTER QUERY PARAMS

--- server.go
+++ server.go
@@ -2,28 +2,53 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
 
 type JsonTeste struct {
-	Author  string `json:"author"`
-	Project string `json:"project"`
+	ID        string `json:"id"`
+	Link      string `json:"link"`
+	RerouteTo string `json:"rerouteLink"`
+}
+
+func randSeq(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
 
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
-	teste := JsonTeste{
-		Author:  "mablds",
-		Project: "github.com/mablds/url-shortner",
+	rand.Seed(time.Now().UnixNano())
+	link, ok := r.URL.Query()["link"] // getting query params values, Query() returns a tuple with value and bool
+
+	if !ok || len(link[0]) < 1 {
+		id := strings.TrimPrefix(r.URL.Path, "/")
+		log.Println(id)
 	}
 
-	fmt.Println(r)
+	linkParam := link[0]
+	log.Println("Url Param 'link' is: " + string(linkParam))
+
+	var randomID string = randSeq(4)
+	linkToRedirect := "localhost:8080/" + randomID
+
+	teste := JsonTeste{
+		ID:        string(randomID),
+		Link:      string(linkParam),
+		RerouteTo: linkToRedirect,
+	}
 
 	jsonBytes, err := json.Marshal(teste)
 	if err != nil {
-
+		log.Println("Error parsing to JSON using Marshal")
 	}
-
 	w.Write(jsonBytes)
 }
 
